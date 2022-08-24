55386c39eb78de47e61d9b75fca2908986598796
mrcoalba <mrcoalba@gmail.com>
2022-08-24 00:01:17 -0500

    Main: Release 0

---
+++ app/src/main/java/the/goats/tracedent/views/MainActivity.kt
@@ -0,0 +1,11 @@
+package the.goats.tracedent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
