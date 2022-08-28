2486273799f83ed4612f44d4cde34338da493ecb
Franco <franco1.alfra@gmail.com>
2022-08-28 16:09:17 -0500

    added base map

--- app/src/main/java/the/goats/tracedent/views/activities/MainActivity.kt
+++ app/src/main/java/the/goats/tracedent/views/activities/MainActivity.kt
@@ -1,16 +1,99 @@
 package the.goats.tracedent.views.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import the.goats.tracedent.R
 import the.goats.tracedent.databinding.ActivityMainBinding
 import the.goats.tracedent.views.base.BaseActivity
 import the.goats.tracedent.views.fragments.LoginFragment
 
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) , OnMapReadyCallback{
+
+    private lateinit var mMap : GoogleMap
+
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        transactionFirstAndMainFragment(LoginFragment(), binding.fcvMainActivity)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        enableLocationOnMap()
+
+    }
+
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun enableLocationOnMap(){
+        if(!::mMap.isInitialized) return
+        if(isLocationPermissionGranted()){
+            mMap.isMyLocationEnabled = true
+        }else{
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this,"Ve a ajustes y acepta los permisos",Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                mMap.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(this,"Ve a ajustes y acepta los permisos",Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Log.d("dasda","FFFFFFFFFF")}
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if(!::mMap.isInitialized) return
+        if(!isLocationPermissionGranted()){
+            mMap.isMyLocationEnabled = false
+            Toast.makeText(this,"Ve a ajustes y acepta los permisos",Toast.LENGTH_SHORT).show()
+        }
+    }
 }
