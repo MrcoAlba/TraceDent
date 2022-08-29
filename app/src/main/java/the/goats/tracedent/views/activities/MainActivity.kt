1d39a55b70b3e4798ae6d29b47a349c0215a657b
Franco <franco1.alfra@gmail.com>
2022-08-28 20:26:11 -0500

    added basic bottom nav bar

--- app/src/main/java/the/goats/tracedent/views/activities/MainActivity.kt
+++ app/src/main/java/the/goats/tracedent/views/activities/MainActivity.kt
@@ -1,6 +1,8 @@
 package the.goats.tracedent.views.activities
 
 import android.os.Bundle
+import android.util.Log
+import the.goats.tracedent.R
 import the.goats.tracedent.databinding.ActivityMainBinding
 import the.goats.tracedent.views.base.BaseActivity
 import the.goats.tracedent.views.fragments.LoginFragment
@@ -11,6 +13,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         super.onCreate(savedInstanceState)
         transactionFirstAndMainFragment(LoginFragment(), binding.fcvMainActivity)
 
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                //Here goes the transitions between fragments, each lines performs when an item is pressed
+
+                R.id.booking_item -> Log.d("BtmNavView", "Se presiono para pasar a las reservas")
+                R.id.search_item -> Log.d("BtmNavView", "Se presiono para pasar a la busqueda")
+                R.id.map_item -> Log.d("BtmNavView", "Se presiono para pasar al mapa(principal")
+                R.id.profile_item -> Log.d("BtmNavView", "Se presiono para pasar al perfil")
+                R.id.messaging_item -> Log.d("BtmNavView", "Se presiono para pasar a los chats")
+
+                else -> {}
+            }
+
+            true
+        }
     }
 
 }
