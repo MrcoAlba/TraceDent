3ac72cf1a3161cc2e285ccacfcd3b61ad8168f73
mrcoalba <mrcoalba@gmail.com>
2022-08-30 21:27:03 -0500

    Testing 2

--- app/src/main/java/the/goats/tracedent/views/fragments/LoginFragment.kt
+++ app/src/main/java/the/goats/tracedent/views/fragments/LoginFragment.kt
@@ -2,10 +2,16 @@ package the.goats.tracedent.views.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import the.goats.tracedent.databinding.FragmentLoginBinding
 import the.goats.tracedent.views.base.BaseFragment
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,7 +21,28 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
     }
 
     private fun login() {
-        
+        auth = Firebase.auth
+        val email = binding.tietEmail.text.toString()
+        val password = binding.tietPassword.text.toString()
+        Toast.makeText(fragmentActivity, email, Toast.LENGTH_SHORT).show()
+        /*
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+
+        */
     }
 
 }
