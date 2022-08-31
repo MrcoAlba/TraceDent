c448e361a124995813941690ce67c5800ba30019
mrcoalba <mrcoalba@gmail.com>
2022-08-30 21:33:49 -0500

    Firebase auth test

--- app/src/main/java/the/goats/tracedent/views/fragments/LoginFragment.kt
+++ app/src/main/java/the/goats/tracedent/views/fragments/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import the.goats.tracedent.databinding.FragmentLoginBinding
@@ -24,25 +25,25 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
         auth = Firebase.auth
         val email = binding.tietEmail.text.toString()
         val password = binding.tietPassword.text.toString()
-        Toast.makeText(fragmentActivity, email, Toast.LENGTH_SHORT).show()
-        /*
         auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
+            .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.makeText(fragmentActivity, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
-                    updateUI(null)
                 }
             }
 
-        */
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        user?.run {
+            Toast.makeText(fragmentActivity, this.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
