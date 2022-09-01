663133c6520d373c67416213071f95695e5159e6
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-08-31 22:42:32 -0500

    OK2

--- app/src/main/java/the/goats/tracedent/views/activities/LoginActivity.kt
+++ app/src/main/java/the/goats/tracedent/views/activities/LoginActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import the.goats.tracedent.databinding.ActivityLoginBinding
 import the.goats.tracedent.views.base.BaseActivity
 import the.goats.tracedent.views.fragments.RegisterG2Fragment
+import the.goats.tracedent.views.fragments.RegisterG3Fragment
+import the.goats.tracedent.views.fragments.RegisterG4Fragment
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        transactionFirstAndMainFragment(RegisterG2Fragment(), binding.fcvLoginActivity)
+        transactionFirstAndMainFragment(RegisterG4Fragment(), binding.fcvLoginActivity)
     }
 
 }
