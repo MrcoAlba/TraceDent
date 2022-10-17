1946689040181ee8f875a50a4f1f2735b65cf7be
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-10-16 19:51:10 -0500

    XML INFO

---
+++ app/src/main/java/the/goats/tracedent/views/fragments/Suscripcion01Fragment.kt
@@ -0,0 +1,40 @@
+package the.goats.tracedent.views.fragments
+
+import android.os.Bundle
+import android.view.View
+import the.goats.tracedent.databinding.FragmentRegisterG0Binding
+import the.goats.tracedent.databinding.FragmentSuscripcion01Binding
+import the.goats.tracedent.databinding.FragmentUsuarioBinding
+import the.goats.tracedent.interfaces.Communicator
+import the.goats.tracedent.views.activities.LoginActivity
+import the.goats.tracedent.views.activities.MainActivity
+import the.goats.tracedent.views.base.BaseFragment
+
+class Suscripcion01Fragment
+    : BaseFragment<FragmentSuscripcion01Binding>(FragmentSuscripcion01Binding::inflate)
+{
+    //This variables are gonna be instantiated on the fragment lifecycle,
+    //At the moment, they are null variables
+    private lateinit var activityParent : MainActivity
+
+
+
+
+    //Fragment Lifecycle
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Delegates
+        communicator    =   requireActivity() as Communicator
+        activityParent  =   requireActivity() as MainActivity
+
+        //Firebase Analytics
+        analyticEvent(requireActivity(), "Suscripcion01Fragment", "onViewCreated")
+
+
+        //Listeners
+
+    }
+
+    //Selected option
+
+}
