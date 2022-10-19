66f1bef15d17f77b0ac0af26ca143acdcc93c6f2
MatiasV-26 <matias.villanueva.paz@gmail.com>
2022-10-19 14:07:14 -0500

    POST usuarios/login

--- app/src/main/java/the/goats/tracedent/interfaces/RetrofitService.kt
+++ app/src/main/java/the/goats/tracedent/interfaces/RetrofitService.kt
@@ -1,14 +1,19 @@
 package the.goats.tracedent.interfaces
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
+import the.goats.tracedent.api.UserLoginResponse
 import the.goats.tracedent.model.Dentist
+import the.goats.tracedent.model.UserPostLogin
 
 interface RetrofitService {
     @GET("dentistas?")
     fun getDentistsList(@Query("name")name:String): Call<MutableList<Dentist>>
     @GET("dentistas")
     fun getAllDentistsList(): Call<MutableList<Dentist>>
-
+    @POST("usuarios/login")
+    fun getUserAccountInfo(@Body userPostLogin: UserPostLogin): Call<UserLoginResponse>
 }
